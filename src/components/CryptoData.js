717b328bcb7418c99014b5c9f7b0a6b12286fede
Nftdysentryanemonemitinity <bryanjeromevaliente@gmail.com>
2022-07-03 13:22:56 -0700

    first commit

--- src/components/CryptoData.js
+++ src/components/CryptoData.js
@@ -3,29 +3,55 @@ import axios from "axios";
 
 const CryptoData = () => {
     // data: represents our data, setdata sets the data we are pulling from the api
-    const [data, setData] = useState([]);
+    const [marketsData, setMarketsData] = useState([]);
+    const [assetsData, setAssetsData] = useState([]);
     // collect user data, empty string
     const baseUrl = "https://www.cryptingup.com/api";
-    
+
     // Make a request to all exchanges
     useEffect(() => {
-        axios.get(`${baseUrl}/exchanges`).then(res => {
-            setData(res.data.exchanges);
+        axios.get(`${baseUrl}/markets`).then(res => {
+            console.log(res);
+            setMarketsData(res.data.markets);
         });
+        axios.get(`${baseUrl}/assets`).then(res => {
+            setAssetsData(res.data.assets);
+        });
+        // axios.get(`${baseUrl}/assets/<ID>/USDT`).then(res => {
+        //     setAssetsData(res.data.assets.id.usdt);
+        // });
     }, []);
-   
+
 
     return (
         <div>
-            <h1>Data from my API</h1>
-            {data.map(x => <li key={x.exchange_id}>{x.name}</li>)}
-            
+            <h1>Data from my API, First Markets and then assets</h1>
             {/* List specific data for the 3 exchanges  */}
-            <h1>Single Post data</h1>
-            <h3>Exchange Name: </h3>
-            <p> </p>
-        </div>  
+            <div>
+                {/* <h1>specific asset</h1>
+                {assetsData.map(x => <li key={x.assets.USDT}>{x.name}</li>)} */}
+             <h1>symbols</h1>
+            {marketsData.map(x => <li key={x.markets}>{x.symbol}</li>)}
+            </div>
+           <div>
+            <h2>markets Id's: </h2>
+            {marketsData.map(x => <li key={x.markets}>{x.exchange_id}</li>)}
+            </div>
+            <div>
+            <h3>Prices:</h3>
+            {marketsData.map(x => <li key={x.markets}>{x.price}</li>)}
+            </div>
+            <div>
+                <h1>Name</h1>
+                {assetsData.map(x => <li key={x.assets}>{x.name}</li>)}
+                <h2>Asset Id's:</h2>
+                {assetsData.map(x => <li key={x.assets}>{x.asset_id}</li>)}
+                <h3>Price: </h3>
+                {assetsData.map(x => <li key={x.assets}>{x.price}</li>)}
+            </div>
+        </div>
     )
 };
 
 export default CryptoData;
+
